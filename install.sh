a6164ef6b1e1dc1ad3f382736689bc213753afae
FreeLinuxOS <44975030+FreeLinuxOS@users.noreply.github.com>
2018-12-11 08:09:15 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ done
  if hash vim-addon  2>/dev/null; then
          echo "vim-addon (vim-scripts)  installed"
      else
-         echo "vim-addon (vim-scripts) not installed, running 'sudo yum update; sudo yum install bc'"
-         sudo yum update; sudo yum install vim-scripts
+         echo "vim-addon (vim-scripts) not installed, running 'sudo yum update; sudo apt install bc'"
+         sudo apt update; sudo apt install vim-scripts
 fi
 
 echo "Installed"
